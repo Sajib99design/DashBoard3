5eac846a13642c6eba19daae537d737075e80fc9
sajib99design <sajib99design@gmail.com>
2023-08-15 20:47:35 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+<script>
+    $(document).ready(function(){
+
+        // $(".bar").click(function(){
+        //     $(this).toggleClass("cloge")
+        //     $(".slick-menu").toggleClass("showmenu");
+        // });
+
+        // $(".protfolio-menu button").click(function(){
+        //     $(this).addClass("pbutton");
+        // });
+        // // inpur click query =======
+        // $(".email-item input").click(function(){
+        //     $(".email-item input").addClass("inputshadow");
+        // });
+
+        // bar-search click search inpur show
+
+        //     $(".nav-search i").click(function () {
+        //         $(".nav-form").toggleClass("searchshow")
+        //     });
+
+        // $(".nav-profile i").click(function () {
+        //     $(".nav-form").toggleClass("searchshow")
+        // });
+
+
+
+    });
+</script>
